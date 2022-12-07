27630c684b948afc281a6f142fa673386cbb2894
Ana Carolina <101834138+ananazario@users.noreply.github.com>
2022-12-07 14:54:38 -0300

    Add files via upload

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import * as C from "./style";
+
+const Header = () => {
+    return (
+        <C.Container>
+            <C.Header>
+                <C.Title>Controle Financeiro</C.Title>
+            </C.Header>
+        </C.Container>
+    )
+}
+
+export default Header;
---
+++ src/styles/global.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+  }
+  
+  body {
+    font-family: 'Poppins', sans-serif;
+    background-color: #f2f2f2;
+`;
+export default GlobalStyle;
+
+
+
